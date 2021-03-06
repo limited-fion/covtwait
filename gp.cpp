3c4767db1fe22a1b52e0802471f5ddc838600b4c
limited-fion <80096703+limited-fion@users.noreply.github.com>
2021-03-06 21:44:21 +0000

    Added gp1 & gp2

--- gp.cpp
+++ gp.cpp
@@ -11,25 +11,37 @@ struct Table_gp{
 };
 
 //add gp detail into vector
-void add(Table_gp gp, std::vector<Table_gp> gp_db){
+void add(Table_gp gp, std::vector<Table_gp>& gp_db){
   gp_db.push_back(gp);
 }
 
 
 int main(){
     
-  Table_gp gp;
+  Table_gp gp1, gp2;
   std::vector<Table_gp> gp_db;
 
-  //input from GP
+  //input from GP1
   std::cout << "GP postcode : ";
-  std::cin >> gp.postcode;
+  std::cin >> gp1.postcode;
  
   std::cout << "Number of vaccines currently available : ";
-  std::cin >> gp.vaccine;
+  std::cin >> gp1.vaccine;
 
-  //add GP details into gp database
-  add(gp, gp_db);
+  //add GP1 details into gp database
+  add(gp1, gp_db);
+
+
+  //input from GP2
+  std::cout << "GP postcode : ";
+  std::cin >> gp2.postcode;
+ 
+  std::cout << "Number of vaccines currently available : ";
+  std::cin >> gp2.vaccine;
+
+  //add GP2 details into gp database
+  add(gp2, gp_db);
+
 
   //store into "gpdetails.txt"
 
